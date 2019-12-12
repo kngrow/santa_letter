d7e87e2a568749eb12581d975d50af2a7a05dfc3
pinon <ymc9.knit.p23@gmail.com>
2019-12-13 00:01:25 +0900

    font loadするまではローディング画面を表示

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import './App.scss';
 import {InputComponent} from './InputComponent.js'
 import {FromComponent} from './FromComponent.js'
-import { Container,Row,Col,Button } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import html2canvas from 'html2canvas';
+
+const FontFaceObserver = require('fontfaceobserver');
 
 class App extends Component {
   constructor() {
@@ -11,16 +13,26 @@ class App extends Component {
     this.state = {
       text: "",
       name: "たかし",
+      isFontLoaded: false,
     }
   }
   updateText = (val) => {
-        this.setState({
-          text : val
-        });
+    this.setState({
+      text : val
+    });
   }
   updateName = (val) => {
     this.setState({
       name:val
+    });
+  }
+  componentDidMount() {
+    const font = new FontFaceObserver('zenzidou');
+    font.load().then( () => {
+      this.setState({
+        'isFontLoaded' : true
+      });
+      console.log('font has loaded');
     });
   }
 
@@ -47,47 +59,62 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>さんたさんにてがみを書くやつ</h1>
-        </header>
-        <Container>
-          <Row>
-            <Col className="how_to_use">
-              <p>さんたさんに<ruby>手<rt>て</rt>紙<rt>がみ</rt></ruby>をかいてみよう！</p>
-            </Col>
-          </Row>
-          <Row>
-            <Col className="form" xs="12" lg="6">
-              <div className="text-left">さんたさんへ</div>
-                <InputComponent onUpdate={ this.updateText }/>
-                <FromComponent onUpdate={ this.updateName }/>
-            </Col>
-            <Col xs="12" lg="6">
+    if (this.state.isFontLoaded) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1>さんたさんにてがみを書くやつ</h1>
+          </header>
+          <Container>
+            <Row>
+              <Col className="how_to_use">
+                <p>さんたさんに<ruby>手<rt>て</rt>紙<rt>がみ</rt></ruby>をかいてみよう！</p>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="form" xs="12" lg="6">
+                <div className="text-left">さんたさんへ</div>
+                  <InputComponent onUpdate={ this.updateText }/>
+                  <FromComponent onUpdate={ this.updateName }/>
+              </Col>
+              <Col xs="12" lg="6">
                 <p className="letter_text">
-                  <span className="dear_santa">さんたさんへ</span>
-                  { this.convertText() }
-                  <span className="from_name">{this.state.name} より</span>
-                </p>
-            </Col>
-          </Row>
-          <Row>
+                    <span className="dear_santa">さんたさんへ</span>
+                    { this.convertText() }
+                    <span className="from_name">{this.state.name} より</span>
+                  </p>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <div className="letter_img"></div>
+                <a href="#" onClick={this.downloadImage} className="Button" download="tegami.png">てがみを保存する</a>
+              </Col>
+            </Row>
+          <Row className="text-white">
             <Col>
-              <div className="letter_img"></div>
-              <a href="#" onClick={this.downloadImage} className="Button" download="tegami.png">てがみを保存する</a>
+              <p>
+                てがみの文字の生成には<a href="https://tanukifont.com/zenjido/" target="_blank">全児童ふぉんと</a> の無料版を使わせていただきました<br />
+              </p>
             </Col>
           </Row>
-        <Row className="text-white">
-          <Col>
-            <p>
-              てがみの文字の生成には<a href="https://tanukifont.com/zenjido/" target="_blank">全児童ふぉんと</a> の無料版を使わせていただきました<br />
-            </p>
-          </Col>
-        </Row>
-        </Container>
-      </div>
-    );
+          </Container>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1>さんたさんにてがみを書くやつ</h1>
+          </header>
+          <Container className="loading">
+             <Row>
+              <div role="status" className="spinner-border text-primary loader"><span className="sr-only">Loading...</span></div>
+              </Row>
+          </Container>
+        </div>
+      );
+    }
   }
 }
 
